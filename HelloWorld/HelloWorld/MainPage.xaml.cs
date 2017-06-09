6e704d918581da6f0b27bdf24ba3b9493aaae219
Eain Phyo Thwe <eainphyothwe@mmrdpub.com>
2017-06-09 14:04:38 +0630

    Add project files.

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        static String bomb = new Random().Next(1, 4).ToString();
+        static int scores = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender,EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded", "Game Over", "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores += 1;
+                await DisplayAlert("Bome Defused!","Scores: "+scores,"Continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }       
+    }
+}
